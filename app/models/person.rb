db6d6f1b17d7d590dc6e9519b693500d16bd6639
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-04-01 16:15:49 +0100

    search

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,10 +2,10 @@ class Person < ApplicationRecord
    has_one_attached :photo,  dependent: :destroy
    validates_presence_of :first, :last, :title, :email, :gender
    scope :get_some, ->(key) {
-       where("title = #{key} ") 
-       .and(where("first = #{key}"))
-       .and(where("last  = #{key}"))
-       .and(where("gender = #{key}"))
-       .and(where("email = #{key}"))
+        where(   "title  = '#{key}' " ) 
+       .or(where("first  = '#{key}' "))
+       .or(where("last   = '#{key}' "))
+       .or(where("gender = '#{key}' "))
+       .or(where("email  = '#{key}' "))
       }
 end
